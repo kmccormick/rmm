e222c469414cc965e5a6623eff49991e7e5e0654
Kevin McCormick <github@mccormicklabs.com>
2017-03-23 15:05:31 -0700

    docker: pull in local_settings.py

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,11 @@ else
 	git clone $GIT_REPO .
 fi
 
+if [[ -r /local_settings.py ]] ; then
+	ln -sf /local_settings.py /code/rmm/local_settings.py
+fi
+
 pip3 install -U -r requirements.txt
 chown -R docker:docker /code
-exec su - docker -c "python3 /code/django/manage.py runserver"
+echo "Starting server..." >&2
+exec su - docker -c "python3 /code/django/manage.py runserver 0.0.0.0:8000"
